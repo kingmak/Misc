36c01ca86101c5a87ec505995fed6160dc98a658
kingmak <kingmak@users.noreply.github.com>
2017-06-02 16:25:25 -0500

    Update printer.py

--- Ascii_tables/printer.py
+++ Ascii_tables/printer.py
@@ -4,9 +4,7 @@ dash = "-"
 space = " "
 
 def center(word, maxLength):
-    topString = "%s%s%s" % (corner, dash * maxLength, border)
-    
-    startIndex = maxLength / len(word)
+    startIndex = maxLength / 2
     cellString = [0] * maxLength
     
     index = 0
@@ -17,20 +15,23 @@ def center(word, maxLength):
         
     index = 0
     while index < maxLength:
-        if type(cellString[index]) == int:
+        if cellString[index] == 0:
             cellString[index] = " "
         index += 1
         
-    print topString
-    print ''.join(cellString)
+    return ''.join(cellString)
 
-#def top(word):
-#    string = "%s%s%splus + len(word)
+def lCell(word, maxLength):
+    topString = "%s%s%s" % (corner, dash * maxLength, border)
+    cellString = "%s%s%s" % (border, center("asdasd", maxLength), border)
+    print(topString)
+    print(cellString)
     
 
 #dashCount = maxLen + 2
 #top = "%s%s%s%s%s" % (corner, line * dashCount, border, line * dashCount, corner)
 #mid = "%s%s%s%s%s" % (border, space * dashCount, border, space * dashCount, border)
 #low = top
-
-(center("asd", 6))
+print("%s%s%s" % (border, center("asdasd", 19), border))
+print(center("asd", 19))
+(lCell("a", 19))
